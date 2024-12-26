c4f0e52d0d52d3e1a531f175c924fd023160796e
samiursakib <rafisamiur@gmail.com>
2024-12-26 21:33:51 +0600

    solved new problems

---
+++ minimum-number-of-operations-to-make-elements-in-array-distinct.rs
@@ -0,0 +1,23 @@
+use std::collections::HashSet;
+
+fn main() {
+    let nums = vec![6,7,8,9];
+    let result = Solution::minimum_operations(nums);
+    println!("Result: {}", result);
+}
+
+struct Solution;
+
+
+impl Solution {
+    pub fn minimum_operations(nums: Vec<i32>) -> i32 {
+        let mut store: HashSet<i32> = HashSet::new();
+        for (i, &num) in nums.iter().enumerate().rev() {
+            if store.contains(&num) {
+                return ((i + 1) as f32 / 3.0).ceil() as i32;
+            }
+            store.insert(num);
+        }
+        return 0;
+    }
+}
